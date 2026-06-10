ff85077a0e77efc6ecafbec25947d5d77285ad7c
carlos.cavaletto <carloseduscc@gmail.com>
2026-06-10 15:52:34 -0300

    test: adiciona teste do método buscarPedidoComItensJoinFetch do repository

---
+++ src/test/java/br/com/carloseduscc/projetoestudos/SistemaPedidosJPA/repository/PedidoRepositoryTest.java
@@ -0,0 +1,23 @@
+package br.com.carloseduscc.projetoestudos.SistemaPedidosJPA.repository;
+
+import br.com.carloseduscc.projetoestudos.SistemaPedidosJPA.model.Pedido;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@SpringBootTest
+public class PedidoRepositoryTest {
+    @Autowired
+    PedidoRepository repository;
+
+
+    @Test
+    void buscarPedidosPorUsuarioTest(){
+        Optional<Pedido> pedidoOpt = repository.buscarPedidoComItensJoinFetch(UUID.fromString("d4f8c6a9-7b3e-4f1a-a29d-8e5b0c1f73ab"));
+
+        IO.println(pedidoOpt.get());
+    }
+}
