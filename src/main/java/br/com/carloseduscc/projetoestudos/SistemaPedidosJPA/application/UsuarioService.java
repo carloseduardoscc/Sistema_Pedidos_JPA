adae2a20b1256f99420a93279711e27dae3a3a27
Carlos Eduardo <carloseduscc@gmail.com>
2026-06-11 13:52:07 -0300

    RF-3 Implementa endpoints de pesquisa paginada e busca de usuários
    * feat: implementa endpoint de busca detalhada de usuário
    
    * feat: implementa endpoint de pesquisa paginada de usuários

--- src/main/java/br/com/carloseduscc/projetoestudos/SistemaPedidosJPA/application/UsuarioService.java
+++ src/main/java/br/com/carloseduscc/projetoestudos/SistemaPedidosJPA/application/UsuarioService.java
@@ -4,14 +4,23 @@ import br.com.carloseduscc.projetoestudos.SistemaPedidosJPA.application.comand.C
 import br.com.carloseduscc.projetoestudos.SistemaPedidosJPA.application.dto.UsuarioDTO;
 import br.com.carloseduscc.projetoestudos.SistemaPedidosJPA.application.mapper.UsuarioMapper;
 import br.com.carloseduscc.projetoestudos.SistemaPedidosJPA.application.validator.UsuarioValidator;
+import br.com.carloseduscc.projetoestudos.SistemaPedidosJPA.exception.NaoEncontradoException;
 import br.com.carloseduscc.projetoestudos.SistemaPedidosJPA.model.Usuario;
 import br.com.carloseduscc.projetoestudos.SistemaPedidosJPA.repository.UsuarioRepository;
+import br.com.carloseduscc.projetoestudos.SistemaPedidosJPA.repository.specs.UsuarioSpecs;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -24,7 +33,7 @@ public class UsuarioService {
     final private UsuarioValidator validator;
 
     @Transactional
-    public UsuarioDTO cadastrarUsuario(CadastrarUsuarioCommand usuarioCmd){
+    public UsuarioDTO cadastrarUsuario(CadastrarUsuarioCommand usuarioCmd) {
 
         Usuario usuario = mapper.fromCommand(usuarioCmd);
 
@@ -32,8 +41,26 @@ public class UsuarioService {
 
         Usuario usuarioSalvo = repository.save(usuario);
 
-        logger.atInfo().log("Usuário "+usuario.getId().toString()+" cadastrado");
+        logger.atInfo().log("Usuário " + usuario.getId().toString() + " cadastrado");
 
         return mapper.toDTO(usuarioSalvo);
+    }
+
+    public UsuarioDTO buscarDetalhes(UUID id) {
+        Optional<Usuario> usuarioOpt = repository.findById(id);
+        Usuario usuario = usuarioOpt.orElseThrow(() -> new NaoEncontradoException("Não existe usuário com id: " + id.toString()));
+        return mapper.toDTO(usuario);
+    }
+
+    public Page<UsuarioDTO> pesquisarListagem(String nome, String email, Integer numeroPagina, Integer tamanhoPagina){
+        Specification<Usuario> usuarioSpecs = UsuarioSpecs.initialize();
+
+        if (nome != null) usuarioSpecs = usuarioSpecs.and(UsuarioSpecs.nomeLike(nome));
+        if (email != null) usuarioSpecs = usuarioSpecs.and(UsuarioSpecs.emailLike(email));
+
+        PageRequest pageable = PageRequest.of(numeroPagina, tamanhoPagina);
+
+        return repository.findAll(usuarioSpecs, pageable).map(mapper::toDTO);
+
     }
 }
