ced4941d7ab8adb162ba75b0ff4616a5c12febd4
Carlos Eduardo <carloseduscc@gmail.com>
2026-06-21 19:49:10 -0300

    feat: cria endpoint para atualizar item

--- src/main/java/br/com/carloseduscc/projetoestudos/SistemaPedidosJPA/api/controller/ItemPedidoController.java
+++ src/main/java/br/com/carloseduscc/projetoestudos/SistemaPedidosJPA/api/controller/ItemPedidoController.java
@@ -2,6 +2,8 @@ package br.com.carloseduscc.projetoestudos.SistemaPedidosJPA.api.controller;
 
 import br.com.carloseduscc.projetoestudos.SistemaPedidosJPA.application.ItemPedidoService;
 import br.com.carloseduscc.projetoestudos.SistemaPedidosJPA.api.controller.common.GenericController;
+import br.com.carloseduscc.projetoestudos.SistemaPedidosJPA.application.comand.AtualizarItemCommand;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -24,6 +26,12 @@ public class ItemPedidoController implements GenericController {
     @DeleteMapping("{id}")
     public ResponseEntity<Object> removerItem(@PathVariable UUID id){
         service.removerItem(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PatchMapping("{id}")
+    public ResponseEntity<Object> atualizarItem(@PathVariable UUID id, @RequestBody @Valid AtualizarItemCommand dados){
+        service.atualizarItem(dados, id);
         return ResponseEntity.noContent().build();
     }
 }
