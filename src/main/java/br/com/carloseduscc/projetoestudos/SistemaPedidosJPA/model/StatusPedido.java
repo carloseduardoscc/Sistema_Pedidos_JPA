baa9b6b21dca3618d862318531dd5f5c6f836714
Carlos Eduardo <carloseduscc@gmail.com>
2026-06-21 20:38:04 -0300

    feat: implementa máquina de estado de status do pedido e validação para transição correta

--- src/main/java/br/com/carloseduscc/projetoestudos/SistemaPedidosJPA/model/StatusPedido.java
+++ src/main/java/br/com/carloseduscc/projetoestudos/SistemaPedidosJPA/model/StatusPedido.java
@@ -1,9 +1,25 @@
 package br.com.carloseduscc.projetoestudos.SistemaPedidosJPA.model;
 
+import br.com.carloseduscc.projetoestudos.SistemaPedidosJPA.model.exception.RegraDeNegocioException;
+import jdk.jshell.Snippet;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+@RequiredArgsConstructor
 public enum StatusPedido {
-    PENDENTE,
-    PAGO,
-    CANCELADO,
-    ENVIADO,
-    RECEBIDO
+    CANCELADO(List.of()),
+    RECEBIDO(List.of()),
+    ENVIADO(List.of(RECEBIDO)),
+    PAGO(List.of(ENVIADO, CANCELADO)),
+    PENDENTE(List.of(PAGO, CANCELADO));
+
+    public final List<StatusPedido> proximosEstadosPossiveis;
+
+    public void validarTransacao(StatusPedido novoStatus){
+        if (!proximosEstadosPossiveis.contains(novoStatus)) lancarErro(novoStatus);
+    }
+    public void lancarErro(StatusPedido status){
+        throw new RegraDeNegocioException("Não é possível tornar %s um pedido que está %s".formatted(status.toString().toLowerCase(), this.toString().toLowerCase()));
+    }
 }
