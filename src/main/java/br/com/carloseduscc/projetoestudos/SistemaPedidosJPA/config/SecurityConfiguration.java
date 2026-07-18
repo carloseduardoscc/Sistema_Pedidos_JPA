26796507810c96d953989742df4c53082d4fcff2
Carlos Eduardo <carloseduscc@gmail.com>
2026-07-18 19:34:49 -0300

    chore: configura OpenAPI e o Swagger no projeto
    - Desabilita geração automática de respostas da Api
    - Marca o filter chain para ignorar requisições ao Swagger
    - Cria configuração para definir informações gerais sobre o projeto via Swagger

--- src/main/java/br/com/carloseduscc/projetoestudos/SistemaPedidosJPA/config/SecurityConfiguration.java
+++ src/main/java/br/com/carloseduscc/projetoestudos/SistemaPedidosJPA/config/SecurityConfiguration.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.core.GrantedAuthorityDefaults;
 import org.springframework.security.core.userdetails.User;
@@ -60,26 +61,24 @@ public class SecurityConfiguration {
     }
 
     @Bean
+    public WebSecurityCustomizer webSecurityCustomizer(){
+        return web -> web.ignoring().requestMatchers(
+                "/v2/api-docs/**",
+                "/v3/api-docs/**",
+                "/swagger-resources/**",
+                "/swagger-ui.html",
+                "/swagger-ui/**",
+                "/webjars/**"
+        );
+    }
+
+    @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder(10);
     }
 
 //    @Bean
     public UserDetailsService userDetailsService(UsuarioService usuarioService, PasswordEncoder encoder) {
-//        UserDetails user1 = User.builder()
-//                .username("admin@email.com")
-//                .password(encoder.encode("321"))
-//                .roles("ADMIN")
-//                .build();
-//
-//        UserDetails user2 = User.builder()
-//                .username("user@email.com")
-//                .password(encoder.encode("123"))
-//                .roles("USER")
-//                .build();
-//
-//        return new InMemoryUserDetailsManager(user1, user2);
-
         return new CustomUserDetailsService(usuarioService);
     }
 
