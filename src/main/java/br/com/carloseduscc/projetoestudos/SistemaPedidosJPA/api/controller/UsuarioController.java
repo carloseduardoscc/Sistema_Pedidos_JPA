f8befba021be40689d58c9bd308dec1b7b0dd8d8
Carlos Eduardo <carloseduscc@gmail.com>
2026-07-18 19:37:08 -0300

    refactor: Elimina Controller de autenticação e move endpoint de registrar usuário para Controller de usuário

--- src/main/java/br/com/carloseduscc/projetoestudos/SistemaPedidosJPA/api/controller/UsuarioController.java
+++ src/main/java/br/com/carloseduscc/projetoestudos/SistemaPedidosJPA/api/controller/UsuarioController.java
@@ -3,7 +3,14 @@ package br.com.carloseduscc.projetoestudos.SistemaPedidosJPA.api.controller;
 import br.com.carloseduscc.projetoestudos.SistemaPedidosJPA.api.controller.common.GenericController;
 import br.com.carloseduscc.projetoestudos.SistemaPedidosJPA.application.UsuarioService;
 import br.com.carloseduscc.projetoestudos.SistemaPedidosJPA.application.comand.AtualizarDadosUsuarioCommand;
+import br.com.carloseduscc.projetoestudos.SistemaPedidosJPA.application.comand.CadastrarUsuarioCommand;
+import br.com.carloseduscc.projetoestudos.SistemaPedidosJPA.application.dto.pedido.AbrirPedidoResponseDTO;
 import br.com.carloseduscc.projetoestudos.SistemaPedidosJPA.application.dto.usuario.UsuarioDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -20,6 +27,22 @@ import java.util.UUID;
 public class UsuarioController implements GenericController {
 
     private final UsuarioService service;
+
+    @PostMapping
+    @Operation(
+            summary = "Cadastro", description = "Realiza cadastro de usuários"
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "203", description = "Cadastro realizado com sucesso"),
+            @ApiResponse(responseCode = "409", description = "O e-mail já está sendo utilizado", content = @Content()),
+            @ApiResponse(responseCode = "422", description = "Erro de validação", content = @Content()),
+            @ApiResponse(responseCode = "403", description = "Apenas usuários administradores podem cadastrar novos funcionários", content = @Content())
+    })
+    public ResponseEntity<UsuarioDTO> cadastrar(@RequestBody @Valid CadastrarUsuarioCommand usuarioCmd){
+        UsuarioDTO usuarioSalvo = service.cadastrarUsuario(usuarioCmd);
+        URI uri = gerarHeaderLocation(usuarioSalvo.id().toString());
+        return ResponseEntity.created(uri).body(usuarioSalvo);
+    }
 
     @PostMapping("{usuarioId}/pedidos")
     @PreAuthorize("#usuarioId == authentication.principal.id or hasRole('ADMIN')")
